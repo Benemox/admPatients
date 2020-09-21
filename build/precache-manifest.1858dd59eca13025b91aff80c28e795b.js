106e84b63b5c858784f30a555f28d61224bbf350
Abemox <66254191+Benemox@users.noreply.github.com>
2020-09-21 19:40:59 +0200

    Add files via upload

---
+++ build/precache-manifest.1858dd59eca13025b91aff80c28e795b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aed63c236653cb83bc5dedf261b375c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "686ab5997323f3687574",
+    "url": "/static/css/main.4c798faf.chunk.css"
+  },
+  {
+    "revision": "613130b13ad0bf07e7b5",
+    "url": "/static/js/2.139aa8b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.139aa8b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "686ab5997323f3687574",
+    "url": "/static/js/main.b25a40bf.chunk.js"
+  },
+  {
+    "revision": "9078284b3c5a7cc4441c",
+    "url": "/static/js/runtime-main.3c7fdb96.js"
+  }
+]);
